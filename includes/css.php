ca41c03ed5add4b2627c618cf6618961ef89c26e
Billy Marte <billy@upteamco.com>
2018-07-29 16:28:11 +0800

    Update css

--- includes/css.php
+++ includes/css.php
@@ -1,1 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style type="text/css">
+	.input-group button{margin: -5px 0px 0px 15px;}
+	.addDivColumn{float:right;}
+</style>
